605998b8360bf93e77fe0123e8681042a22bc06e
Yash Kolambekar <41070233+yashkolambekar@users.noreply.github.com>
2022-12-31 19:39:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function youtubeopen(){
+const yt = document.getElementById('youtube-content');
+console.log(yt);
+yt.addEventListener('click', function(){
+    window.open("https://www.youtube.com/watch?v=tolPBtRq0pE");
+  });
+}
